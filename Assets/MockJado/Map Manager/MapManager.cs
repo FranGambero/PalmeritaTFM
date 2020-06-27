0ec19ff618be13d2a42315761b1eb800050f0fe9
Ignacio Aparicio Blanco <ignapa12@gmail.com>
2020-06-27 19:04:50 +0200

    feat: spawn mapa base  (mockup)

---
+++ Assets/MockJado/Map Manager/MapManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ElJardin
+{
+    public class MapManager : MonoBehaviour
+    {
+        #region Variables
+
+        /**
+         * 
+         */
+        [Header("Matrix Size")]
+        public int rows, columns;
+
+        [Header("Map Correction")]
+        public float tileOffset;
+
+        /**
+         * 
+         */
+        [Header("Tile Materials")]
+        public Material groundMat;
+
+        public Material waterMat;
+        //public Material waterMatStr8, waterMatCurve, waterMatFork, waterMatCross, waterMatEnd;
+
+        public Material grooveMat;
+        //public Material grooveMatStr8, grooveMatCurve, grooveMatFork, grooveMatCross, grooveMatEnd;
+
+
+        [Header("Node Prefab")]
+        public GameObject nodePrefab;
+        /**
+         * 
+         */
+        [Header("Start/End")]
+        public Vector2 riverStartPos;
+        public Vector2 riverEndPos;
+        public Vector2 playerStartPos;
+
+        private GameObject[,] mapMatrix;
+        #endregion
+
+        private void Awake()
+        {
+            mapMatrix = new GameObject[rows,columns];
+        }
+
+        private void Start()
+        {
+            InitializeMap();
+        }
+
+        #region Matrix
+
+        public void InitializeMap()
+        {
+            for(int i = 0; i<rows; i++)
+            {
+                for(int j = 0; j < columns; j++)
+                {
+                    mapMatrix[i,j] = Instantiate(nodePrefab, new Vector3(j,0,i)*tileOffset, Quaternion.Euler(-90,0,0), this.transform);
+                    mapMatrix[i, j].GetComponent<Node>().ChangeNodeType(NodeType.Ground, groundMat);
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ Assets/MockJado/Map Manager/Node.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ElJardin
+{
+    public class Node : MonoBehaviour
+    {
+        #region Variables
+
+        private Material currentMaterial;
+        private NodeType nodeType;
+
+        private MeshRenderer _mr;
+
+        #endregion
+
+        private void Awake()
+        {
+            nodeType = NodeType.Undefined;
+
+            _mr = GetComponent<MeshRenderer>();
+        }
+
+        public void ChangeNodeType(NodeType newType, Material newMaterial)
+        {
+            nodeType = newType;
+            _mr.material = newMaterial;
+        }
+    }
+}
