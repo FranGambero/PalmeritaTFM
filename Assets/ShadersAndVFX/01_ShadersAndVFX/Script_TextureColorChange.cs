1d2de26c46f1092db453ce39c9cfa2ebbf42c0fa
sarapl <saraperezlantes@gmail.com>
2020-07-09 19:23:05 +0200

    Cambio de materiales e iluminación

---
+++ Assets/ShadersAndVFX/01_ShadersAndVFX/Script_TextureColorChange.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_TextureColorChange : MonoBehaviour
+{
+    public Renderer mrenderer;
+    public Color SelectedColor;
+    public Color NotSelectedColor;
+
+    MaterialPropertyBlock materialPropertyBlock;
+
+    private void Awake()
+    {
+        materialPropertyBlock = new MaterialPropertyBlock();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Puse dos ifs con pulsar teclas para comprobar si funcionaba, la cosa es que tengáis una condición para que cambie de color y otra para que vuelva al original
+       
+        if (Input.GetKeyDown(KeyCode.Space)) //AQUÍ PONED LA CONDICIÓN PARA QUE CAMBIE DE COLOR (QUE PASE EL RATÓN POR ENCIMA O LO QUE SEA)
+        {
+            mrenderer.GetPropertyBlock(materialPropertyBlock);
+            materialPropertyBlock.SetColor("_Color", SelectedColor);
+            mrenderer.SetPropertyBlock(materialPropertyBlock);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)) //aquí la de que vuelva a lo suyo
+        {
+            mrenderer.GetPropertyBlock(materialPropertyBlock);
+            materialPropertyBlock.SetColor("_Color", NotSelectedColor);
+            mrenderer.SetPropertyBlock(materialPropertyBlock);
+        }
+
+    }
+}
---
+++ Assets/ShadersAndVFX/01_ShadersAndVFX/Shader_TextureColorChange.shader
@@ -0,0 +1,54 @@
+﻿Shader "Unlit/Textura"
+{
+	Properties{
+		_MainTex("Textura", 2D) = "white"{}
+	    [PerRendererData] _Color("Tint",color)=(0,0,0,1)
+	}
+	SubShader{
+		Tags{
+				"RenderType"="Opaque" 
+				"Queue"="Geometry"
+			}
+
+			Blend SrcAlpha OneMinusSrcAlpha // Traditional transparency
+			ZWrite Off
+		Pass{
+			
+
+			CGPROGRAM
+			#include "UnityCG.cginc"
+
+			#pragma vertex vert
+			#pragma fragment frag
+
+			struct appdata{
+				float4 vertex : POSITION;
+				float2 uv: TEXCOORD0;
+			};
+
+			sampler2D _MainTex;
+			float4 _Color;
+			float4 _MainTex_ST; //para tiling e offset
+
+			struct v2f{
+				float4 position : SV_POSITION;
+				float2 uv: TEXCOORD0;
+			};
+
+			v2f vert(appdata v){
+				v2f o;
+				o.position = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv,_MainTex);
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_TARGET{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				col *= _Color;
+				return col;
+			}
+
+			ENDCG
+		}
+	}
+}
