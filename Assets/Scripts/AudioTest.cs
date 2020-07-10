f6c08602acaa948ede8de235fe384a3ec2ca2038
Fran Gambero <frangansal.13@gmail.com>
2020-07-10 18:06:41 +0200

    feat: init branch for Audio

---
+++ Assets/Scripts/AudioTest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTest : MonoBehaviour
+{
+    private void Awake() {
+        // This is a test
+    }
+}
