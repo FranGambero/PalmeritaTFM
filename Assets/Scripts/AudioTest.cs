214bcdfb2ad46de4f305242456b204d603e3b7df
Fran Gambero <frangansal.13@gmail.com>
2020-07-27 18:08:35 +0200

    feat: Evento musica ambiente In y out implementado

--- Assets/Scripts/AudioTest.cs
+++ Assets/Scripts/AudioTest.cs
@@ -2,9 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioTest : MonoBehaviour
-{
-    private void Awake() {
-        // This is a test
+public class AudioTest : MonoBehaviour {
+    private void Start() {
+        // Si va en el Awake peta, esta linea irá en la clase que inicie bien el juego
+        AkSoundEngine.PostEvent("Amb_Base_In", gameObject);
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.P)) {
+            // Para probar el efecto OUT, al darle a la tecla P se para el audio In y entra este en su lugar
+            AkSoundEngine.PostEvent("Amb_Base_Out", gameObject);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            AkSoundEngine.PostEvent("Amb_Base_In", gameObject);
+        }
     }
 }
