aec15137fbe604fc156a56cd5a652d4b87ad503a
Fran Gambero <frangansal.13@gmail.com>
2020-09-01 23:23:17 +0200

    feat: Start and Main Menu creation

---
+++ Assets/MockJado/UI/Scripts/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+    public GameObject titleMenu, startMenu;
+
+    private void Awake() {
+        titleMenu.SetActive(true);
+        startMenu.SetActive(false);
+    }
+
+    private void Update() {
+        if (Input.anyKey && !startMenu.activeSelf) {
+            changeToStartMenu();
+        }
+    }
+
+    public void changeToStartMenu() {
+        titleMenu.SetActive(false);
+        startMenu.SetActive(true);
+    }
+}
