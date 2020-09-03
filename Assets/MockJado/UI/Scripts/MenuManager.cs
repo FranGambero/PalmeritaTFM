77be49a8b1a2c755a23d7b0dc4ebdf6fd59806a2
Fran Gambero <frangansal.13@gmail.com>
2020-09-03 17:39:33 +0200

    feat: added mockup menu and rock changes

--- Assets/MockJado/UI/Scripts/MenuManager.cs
+++ Assets/MockJado/UI/Scripts/MenuManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
     public GameObject titleMenu, startMenu;
@@ -8,6 +9,10 @@ public class MenuManager : MonoBehaviour {
     private void Awake() {
         titleMenu.SetActive(true);
         startMenu.SetActive(false);
+    }
+
+    public void goToGame() {
+        SceneManager.LoadScene(1);
     }
 
     private void Update() {
