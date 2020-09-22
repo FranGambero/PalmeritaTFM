c8a9e375ed2213a3bb9879d0869a329e46e00d23
Fran Gambero <frangansal.13@gmail.com>
2020-09-22 13:46:38 +0200

    feat: started new Semaphore functionality

---
+++ Assets/Scripts/Semaphore/Semaphore.cs
@@ -0,0 +1,34 @@
+﻿using ElJardin;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Semaphore : Singleton<Semaphore> {
+    public List<ITurn> turnBasedElementList;
+    public Action<int> onTurnStart;
+
+    private void Awake() {
+        turnBasedElementList = new List<ITurn>();
+
+        turnBasedElementList.AddRange(FindObjectsOfType<MonoBehaviour>().OfType<ITurn>());
+
+        turnBasedElementList.Sort((E1, E2) => E1.turnIndex.CompareTo(E2.turnIndex));
+
+        foreach (var item in turnBasedElementList) {
+            onTurnStart += item.onTurnStart;
+        }
+    }
+
+    private void onTurnEnd(int index) {
+        int currentIndex;
+
+        var currentITurn = turnBasedElementList.Find(e => e.turnIndex == index);
+        currentIndex = turnBasedElementList.IndexOf(currentITurn);
+        currentIndex = (currentIndex + 1) % turnBasedElementList.Count;
+
+        onTurnStart?.Invoke(turnBasedElementList[currentIndex].turnIndex);      // :(
+    }
+
+}
