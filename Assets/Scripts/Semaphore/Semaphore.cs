f001b7205aec69f42d004335c03017299eff27a4
Fran Gambero <frangansal.13@gmail.com>
2020-09-23 13:41:49 +0200

    feat: Semaphore almost finished!!

--- Assets/Scripts/Semaphore/Semaphore.cs
+++ Assets/Scripts/Semaphore/Semaphore.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class Semaphore : Singleton<Semaphore> {
     public List<ITurn> turnBasedElementList;
     public Action<int> onTurnStart;
+    public int currentTurn;
 
     private void Awake() {
         turnBasedElementList = new List<ITurn>();
@@ -21,14 +22,20 @@ public class Semaphore : Singleton<Semaphore> {
         }
     }
 
-    private void onTurnEnd(int index) {
+    private void Start() {
+        // Se cambiará por otra función que inicie el nivel
+        onTurnStart?.Invoke(currentTurn);
+    }
+
+    public void onTurnEnd(int index) {
         int currentIndex;
 
         var currentITurn = turnBasedElementList.Find(e => e.turnIndex == index);
         currentIndex = turnBasedElementList.IndexOf(currentITurn);
         currentIndex = (currentIndex + 1) % turnBasedElementList.Count;
 
-        onTurnStart?.Invoke(turnBasedElementList[currentIndex].turnIndex);      // :(
+        currentTurn = turnBasedElementList[currentIndex].turnIndex;
+        onTurnStart?.Invoke(currentTurn);      // :(
     }
 
 }
