df15e21d4041991bf2c4fd97cb437d5472b27d12
Fran Gambero <frangansal.13@gmail.com>
2020-09-29 14:27:49 +0200

    feat: Card drawing and serializable

--- Assets/MockJado/Cards/Card.cs
+++ Assets/MockJado/Cards/Card.cs
@@ -6,16 +6,27 @@ using UnityEngine.UI;
 
 namespace ElJardin {
     public class Card : MonoBehaviour {
-        public int amount;
-        public DirectionType direction;
+        public CardData cardData;
         public TextMeshProUGUI amountText;
+        public int transformIndex;
+        public bool usedCard;
+
+        private void Awake() {
+            usedCard = false;
+        }
 
         private void Start() {
             loadCardData();
         }
 
-        private void loadCardData() {
-            amountText.text = amount.ToString();
+        public void loadCardData() {
+            amountText.text = cardData.amount.ToString();
+        }
+
+        public void changeCardTransform(int newIndex) {
+            transformIndex = newIndex;
+            this.transform.position = CardManager.Instance.transformList[transformIndex].position;
+            transform.SetParent(CardManager.Instance.transformList[transformIndex]);
         }
 
         private DirectionType getNewDirection() {
--- Assets/MockJado/Cards/CardManager.cs
+++ Assets/MockJado/Cards/CardManager.cs
@@ -1,11 +1,69 @@
 ﻿using ElJardin;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardManager : MonoBehaviour
-{
-    public List<Card> cardList;
+namespace ElJardin {
+    public class CardManager : Singleton<CardManager> {
+        [SerializeField]
+        public Queue<CardData> cardQueue;
+        [SerializeField]
+        public List<CardData> cardList;
+        public List<Card> handList;
+        public List<Transform> transformList;
+        public int maxHand;
+        public Card cardPrefab;
 
+        private void Awake() {
+            maxHand = 5;
+            handList = new List<Card>();
+            cardQueue = new Queue<CardData>(cardList);
+        }
 
+        private void Start() {
+            firstDrawCard();
+        }
+
+        private void firstDrawCard() {
+            int index = 0;
+            while (index < maxHand - 1) {
+                Card tmpCard;
+                tmpCard = Instantiate(cardPrefab, transformList[index]);
+                handList.Add(tmpCard);
+                handList[index].cardData = cardQueue.Dequeue();
+                handList[index].loadCardData();
+                handList[index].changeCardTransform(index);
+                index++;
+            }
+        }
+
+        public void moveCards(int currentIndex) {
+            Card tmpCard = handList.Find(c => c.transformIndex == currentIndex);
+            handList.ForEach(c => {
+                if (c.transformIndex > currentIndex) {
+                    c.changeCardTransform(c.transformIndex - 1);
+                }
+            });
+
+            tmpCard.changeCardTransform(maxHand - 1);
+        }
+
+        public void drawNextCard() {
+            Debug.LogWarning("QUE ROBO EH" + cardQueue.Count);
+            if (cardQueue.Count > 0) {
+                int lastHandIndex = maxHand - 1;
+                Card tmpCard = handList.Find(c => !c.gameObject.activeSelf);
+                if(tmpCard == null) {
+                    tmpCard = Instantiate(cardPrefab, transformList[lastHandIndex]);
+                    handList.Add(tmpCard);
+                }
+                Debug.LogWarning("La cartica eh, " + tmpCard);
+                tmpCard.cardData = cardQueue.Dequeue();
+                tmpCard.loadCardData();
+                tmpCard.changeCardTransform(lastHandIndex);
+                tmpCard.gameObject.SetActive(true);
+            }
+        }
+    }
 }
