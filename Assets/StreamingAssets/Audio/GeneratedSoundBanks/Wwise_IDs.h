46ec2d3d423cde984c9c94fb2ca26fc0fb8e6c98
Eduardo <eduardo@sowoodrec.studio>
2020-11-01 09:06:54 +0300

    Wwise_Posicion_Carta
    Sonido/acorde de posicion carta

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -15,6 +15,7 @@ namespace AK
     {
         static const AkUniqueID AMB_BASE_IN = 430716873U;
         static const AkUniqueID AMB_BASE_OUT = 941055080U;
+        static const AkUniqueID CARTA_POSICION_IN = 2289756239U;
         static const AkUniqueID CARTA_SELECT_IN = 236399107U;
         static const AkUniqueID CARTA_SLIDE_IN = 2760388952U;
         static const AkUniqueID HORMIGA_ATAQUE_IN = 1694954324U;
