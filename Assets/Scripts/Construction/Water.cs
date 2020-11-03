b915715691dc635907edca5749c58972b5edea44
JesusLX <jpmx96@gmail.com>
2020-11-03 12:04:03 +0100

    Add loto con controlador de animaciones y las formas del agua

--- Assets/Scripts/Construction/Water.cs
+++ Assets/Scripts/Construction/Water.cs
@@ -1,57 +1,91 @@
-﻿using System.Collections;
+﻿using ElJardin;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Water : MonoBehaviour {
-    public float valuey = 0.4f;
-    public float valuex = 0;
-    public float maxValue = 1.5f;
+
+    public float maxValue = 1f;
     public float minValue = 0f;
     public bool growing = false;
-    private void Update() {
-        transform.localScale = new Vector3(valuex, valuey, 1.5f);
-    }
+    public GameObject waterN, waterW, waterE, waterS, waterStatic;
+
     [ContextMenu("Grow")]
-    public void Grow(bool grow, System.Action middleCallback) {
-        if (!growing)
-            StartCoroutine(CorGrow(grow, middleCallback));
-    }
-    private IEnumerator CorGrow(bool grow, System.Action middleCallback) {
-        float targetVal = 0;
-        bool invoked = false;
-        growing = true;
-        float call = .9f;
-        if (!grow) {
-            if (valuex > minValue) {
-                targetVal = minValue;
-                while (minValue < valuex) {
-                    valuex -= .1f;
-                    if (!invoked && valuex >= call) {
-                        invoked = true;
-                        middleCallback?.Invoke();
-                    }
-
-                    yield return new WaitForEndOfFrame();
-                }
-                // valuey = 0;
-                valuex = 0;
+    public void Grow(bool grow, System.Action middleCallback, Node initNode) {
+        if (!growing) {
+            if (initNode == null) {
+                initNode = GetComponentInParent<Node>();
             }
+            Node thisNode = GetComponentInParent<Node>();
+            Rotate(thisNode);
 
-        } else {
-            if (valuex < maxValue) {
-                targetVal = maxValue;
-                valuex = 0;
-                while (maxValue > valuex) {
-                    valuex += .01f;
-                    if (!invoked && valuex >= call) {
-                        Debug.Log(valuex);
-                        invoked = true;
-                        middleCallback?.Invoke();
-                    }
-                    yield return new WaitForEndOfFrame();
-                }
+            if (initNode.column == thisNode.column && initNode.row > thisNode.row) {
+                StartCoroutine(CorGrow(grow, middleCallback, waterN));
+            } else
+            if (initNode.column > thisNode.column && initNode.row == thisNode.row) {
+                StartCoroutine(CorGrow(grow, middleCallback, waterE));
+            } else
+            if (initNode.column < thisNode.column && initNode.row == thisNode.row) {
+                StartCoroutine(CorGrow(grow, middleCallback, waterW));
+            } else
+            if (initNode.column == thisNode.column && initNode.row < thisNode.row) {
+                StartCoroutine(CorGrow(grow, middleCallback, waterS));
+            } else {
+                StartCoroutine(CorGrow(grow, middleCallback, waterS));
             }
         }
+    }
+
+    public void Rotate(Node thisNode) {
+        transform.SetParent(null);
+        transform.rotation = Quaternion.identity;
+        transform.SetParent(thisNode.transform);
+    }
+
+    private IEnumerator CorGrow(bool grow, System.Action middleCallback, GameObject water) {
+        float mayor, menor, incremento, valueZ, callTriger = .8f;
+        bool invoked = false;
+        growing = true;
+        if (!waterStatic.activeSelf) {
+            if (grow) {
+                water.SetActive(true);
+                valueZ = 0;
+                mayor = maxValue;
+                menor = valueZ;
+                incremento = .01f;
+                while (mayor >= menor) {
+                    valueZ += incremento;
+                    if (!invoked && valueZ >= callTriger) {
+                        invoked = true;
+                        middleCallback?.Invoke();
+                    }
+                    water.transform.localScale = new Vector3(1, 1f, valueZ);
+                    yield return new WaitForEndOfFrame();
+                    if (grow) {
+                        menor = valueZ;
+                    } else {
+                        mayor = valueZ;
+                    }
+                }
+            } else {
+                valueZ = 1;
+                mayor = valueZ;
+                menor = minValue;
+                incremento = -.1f;
+
+                water.transform.localScale = new Vector3(1, 1f, valueZ);
+            }
+
+            waterStatic.SetActive(grow);
+            water.SetActive(false);
+
+        } else {
+            yield return new WaitForSeconds(.1f);
+
+            middleCallback?.Invoke();
+
+        }
         growing = false;
+
     }
 }
