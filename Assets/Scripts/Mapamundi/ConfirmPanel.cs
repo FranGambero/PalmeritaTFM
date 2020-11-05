c9a531b5ec8cc7c56067e85970a956c8b7011143
Fran Gambero <frangansal.13@gmail.com>
2020-11-05 14:39:55 +0100

    feat: Mockup Mapamundi y esqueleto

---
+++ Assets/Scripts/Mapamundi/ConfirmPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ConfirmPanel : MonoBehaviour
+{
+    public string levelToLoad;
+    public TextMeshProUGUI levelName;
+
+    private void Start() {
+        if (this.gameObject.activeSelf) {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private void Update() {
+        if(this.gameObject.activeSelf && Input.GetKeyDown(KeyCode.Escape)) {
+            Cancel();
+        }
+    }
+
+    void Cancel() {
+        this.gameObject.SetActive(false);
+    }
+
+    public void PlayLevel() {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
---
+++ Assets/Scripts/Mapamundi/LevelButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelButton : MonoBehaviour
+{
+    public Sprite[] petals;
+    public TextMeshProUGUI levelText;
+    public int level;
+    public int numPetals = 0;
+    public ConfirmPanel confirmPanel;
+
+    private void Awake() {
+        if(confirmPanel == null) {
+            //No va :(((
+            confirmPanel = FindObjectOfType<ConfirmPanel>();
+        }
+    }
+
+    private void Start() {
+        ChangeSprite();
+    }
+
+    public void ShowConfirmPanel() {
+        confirmPanel.gameObject.SetActive(true);
+        // No va :(
+        confirmPanel.levelName.text = levelText.text;
+    }
+
+    public void ChangeSprite() {
+        this.gameObject.GetComponent<Image>().sprite = petals[numPetals];
+    }
+}
