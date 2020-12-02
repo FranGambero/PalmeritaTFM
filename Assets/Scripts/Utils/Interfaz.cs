06ce7213d3517b80f2d8ca9687d7a873bf620449
Fran Gambero <frangansal.13@gmail.com>
2020-12-02 12:58:28 +0100

    feat: tweak para cambio escenas de Takox

---
+++ Assets/Scripts/Utils/Interfaz.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class Interfaz : Editor {
+
+    [MenuItem("Escenitas/StartMenu")]
+    static void LoadScene1() {
+        EditorSceneManager.OpenScene("Assets/Scenes/StartMenu.unity", OpenSceneMode.Single);
+    }
+
+    [MenuItem("Escenitas/Mapamundi")]
+    static void LoadScene2() {
+        EditorSceneManager.OpenScene("Assets/Scenes/Mapa.unity", OpenSceneMode.Single);
+    }
+
+    [MenuItem("Escenitas/Level 0")]
+    static void LoadScene3() {
+        EditorSceneManager.OpenScene("Assets/Scenes/Level0.unity", OpenSceneMode.Single);
+    }
+}
