d25210356dbc8eb74d88b224662858847cd97e7f
JesusLX <jpmx96@gmail.com>
2020-12-04 13:55:50 +0100

    Mats Sepalo, Petalos y sus fases, canvas menu mapa

--- Assets/Scripts/Mapamundi/LevelButton.cs
+++ Assets/Scripts/Mapamundi/LevelButton.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class LevelButton : MonoBehaviour {
-    public GameObject[] petals;
+    public List<GameObject> petals;
+    public List<GameObject> pods;
+    public List<GameObject> cocoons;
     public Transform parentElementsToAppears;
     private List<DoGrow> elementsToAppears;
     public GameObject lockedSprite;
@@ -63,33 +65,39 @@ public class LevelButton : MonoBehaviour {
     [ContextMenu("ChangeSprite")]
     public void ChangeSprite() {
         new List<GameObject>(petals).ForEach(p => p.SetActive(false));
-        if (numPetals > 0) {
 
-            for (int i = 0; i < numPetals; i++) {
-                petals[i].SetActive(true);
-            }
-            if (elementsToAppears.Count > 0) {
+        for (int i = 0; i < numPetals; i++) {
+            petals[i].SetActive(true);
+        }
+        if (elementsToAppears.Count > 0) {
+            if (numPetals > 0) {
                 if (SessionVariables.Instance.sceneData.lastScene != confirmPanel.GetLevelBuildId()) {
                     elementsToAppears.ForEach(e => e.QuickGrow());
                 } else {
                     elementsToAppears[0].Grow();
                     elementsToAppears.ForEach(e => e.RandomGrow());
                 }
+            } else {
+                elementsToAppears.ForEach(e => e.QuickShrink());
             }
         }
     }
 
     private void CheckAvailableLevel() {
         //  Para poder empezar en el 1º nivel empezamos a checkear a partir del 2º
-        //if (levelId > 0) {
-        //    LevelData levelData = MapamundiManager.Instance.GetCurrentLevel(levelId - 1);
-        //    isActive = levelData.isCompleted;
+        if (levelId > 0) {
+            LevelData levelData = MapamundiManager.Instance.GetCurrentLevel(levelId - 1);
+            isActive = levelData.isCompleted;
 
-        //    if (!isActive) {
-        //        GetComponent<Button>().interactable = false;
-        //    }
-        //    lockedSprite.SetActive(!isActive);
-        //}
+            if (!isActive) {
+                GetComponent<Button>().interactable = false;
+                petals.ForEach(p => p.SetActive(false));
+            } else {
+            }
+            lockedSprite.SetActive(!isActive);
+            pods.ForEach(p => p.SetActive(!isActive));
+            cocoons.ForEach(c => c.SetActive(isActive));
+        }
 
     }
 }
