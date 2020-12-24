e885b529909864584da4df943afa8ed2133f73d4
JesusLX <jpmx96@gmail.com>
2020-12-24 11:53:28 +0100

    controlAnimatorPala y sprites con sombras

---
+++ Assets/Scripts/Animator/ShovelCravAnimatorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShovelCravAnimatorController : MonoBehaviour
+{
+    public Animator animator;
+    public ParticleSystem RealDigPS, AppearsPS, DisappearsPS;
+    public Renderer bodyMat;
+    public List<Texture> skins;
+    [ContextMenu("DIG")]
+    public void PlayDig() {
+        gameObject.SetActive(true);
+        animator.Play("Dig");
+    }
+  public void OnAnimDig() {
+        bodyMat.material.SetTexture("_MainTex", skins[Random.Range(0, skins.Count)]);
+        RealDigPS?.Play();
+    }
+    public void OnEndAnim() {
+        gameObject.SetActive(false);
+    }
+    public void OnEndDigAnim() {
+        DisappearsPS?.Play();
+    }
+    public void OnStartAnim() {
+        AppearsPS?.Play();
+    }
+}
